18960c3d2ae5908e09351496ab1aaefd99413175
MetrPikeska <piter.mikeska@gmail.com>
2026-03-17 15:52:41 +0100

    refactor vypocitej_vzdalenost_od_rovniku to include unit conversion and rounding options

--- cviceni4/mikeska/geodistance_prijmeni.py
+++ cviceni4/mikeska/geodistance_prijmeni.py
@@ -1,14 +1,25 @@
 
 
-def vypocitej_vzdalenost_od_rovniku(sirka):
-    """Vypočítá vzdálenost od rovníku na základě zeměpisné šířky."""
+def vypocitej_vzdalenost_od_rovniku(sirka: float, jednotka: str = "km", zaokrouhlit: bool = True) -> float:
+   
     KM_NA_STUPEN = 111.32
+    KM_NA_MILI = 0.621371
+    
     vzdalenost = abs(sirka) * KM_NA_STUPEN
-    print(f"Město na šířce {sirka}° je {vzdalenost:.2f} km od rovníku.")
+    
+    if jednotka == "míle":
+        vzdalenost *= KM_NA_MILI
+    
+    if zaokrouhlit:
+        vzdalenost = round(vzdalenost)
+    
     return vzdalenost
 
 
-sirky = [0, 15, 30, 45, 60]
 
+sirky = [0, 1234.4, 30.43434, 45.2 , 60.5, 90]
+
+print("=== Vzdálenosti v kilometrech ===")
 for sirka in sirky:
-    vypocitej_vzdalenost_od_rovniku(sirka)
+    vzdalenost = vypocitej_vzdalenost_od_rovniku(sirka, jednotka="km")
+    print(f"Město na šířce {sirka}° je {vzdalenost} km od rovníku.")
